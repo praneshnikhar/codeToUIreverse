abf3808797bc0d6a2108a066b94dff1aadae9fa5
Pranesh Nikhar <praneshnikhar@gmail.com>
2025-07-27 12:43:11 +0530

    Update reactFiberLookup.js

--- src/reactFiberLookup.js
+++ src/reactFiberLookup.js
@@ -1,13 +1,8 @@
-/**
- * Given a DOM Node selected via the click,
- * traverse React Fiber tree to find matching Fiber node's debug source info.
- * @param {Element} domNode
- * @returns {{ fileName: string, lineNumber: number, columnNumber: number } | null}
- */
+
 function getReactFiberForNode(domNode) {
     if (!domNode) return null;
   
-    // Walk up the DOM tree to find a React root container with _reactRootContainer property
+  
     let rootContainer = domNode;
     while (rootContainer && !rootContainer._reactRootContainer) {
       rootContainer = rootContainer.parentNode;
@@ -18,12 +13,12 @@ function getReactFiberForNode(domNode) {
   
     const rootFiber = rootContainer._reactRootContainer._internalRoot.current;
   
-    // Recursive traverse fiber tree to find fiber with stateNode === domNode and debug source info
+    
     function traverse(fiber) {
       if (!fiber) return null;
   
       if (fiber.stateNode === domNode && fiber._debugSource) {
-        return fiber._debugSource; // { fileName, lineNumber, columnNumber }
+        return fiber._debugSource;
       }
       let res = traverse(fiber.child);
       if (res) return res;
